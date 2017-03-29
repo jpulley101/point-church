a1b9b8232162afbd53b434afd8228fe49e09e50d
Josh Mobley <jmob1986@gmail.com>
2017-03-29 10:39:40 -0400

    refactoring footer/footer styles

--- wp-content/themes/point-church/footer.php
+++ wp-content/themes/point-church/footer.php
@@ -1,18 +1,22 @@
 <footer>
 
-		<h1 class="footer-title">The Point Church</h1>
-	
-		<div class="footer-address">
+		<h1 class="footer__title">The Point Church</h1>
+
+		<div class="footer__address">
 			1503 Walnut St.<br/>
 			Cary, NC 27511<br/>
 			919-467-9159
 		</div><!--.footer-address-->
 
-		<div class="footer-utilities">
+		<div class="buttons">
 
-			<a class="button float-left" href="https://point.ccbchurch.com/">Connect Account</a>
-			<p class="copyright">&copy;2015 The Point Church | <a href="<?php echo bloginfo('url'); ?>/wp-admin">Log In</a></p>
-			<a class="button float-right" href="https://pushpay.com/pay/pointchurch">Online Giving</a>
+			<a href="https://point.ccbchurch.com/" class="button--gray" >Connect Account</a>
+			<a href="https://pushpay.com/pay/pointchurch" class="button--gray">Online Giving</a>
+
+    </div>
+
+
+		<p class="copyright">&copy;2015 The Point Church | <a href="<?php echo bloginfo('url'); ?>/wp-admin">Log In</a></p>
 
 </footer>
 
